ec3520098f72268aac143ae9ccecdfb0746aedcc
13frfr <2458151214@qq.com>
2022-07-08 17:48:12 +0800

    项目初始化-调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <div></div>
+  <div id="app">
+    <!-- 路由的出口 -->
+    <router-view/>
+    <h1>黑马头条</h1>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name:'App'
 }
 </script>
 
-<style>
+<style lang="less">
 
 </style>
